36dc43a17e78c7997579c8f3d15d782b546adea1
frank926 <frank926@163.com>
2018-05-16 12:20:59 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -8,7 +8,7 @@ import org.litepal.crud.DataSupport;
 public class Province extends DataSupport {
     private int id;
     private String provinceName;
-    private String provinceCode;
+    private int provinceCode;
 
     public int getId() {
         return id;
@@ -26,11 +26,11 @@ public class Province extends DataSupport {
         this.provinceName = provinceName;
     }
 
-    public String getProvinceCode() {
+    public int getProvinceCode() {
         return provinceCode;
     }
 
-    public void setProvinceCode(String provinceCode) {
+    public void setProvinceCode(int provinceCode) {
         this.provinceCode = provinceCode;
     }
 }
---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.util;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+/**
+ * Created by lil78 on 2018/5/16.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback) {
+        OkHttpClient client =new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
